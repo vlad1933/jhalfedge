9d8b697820adc65a045bd3ff97bb4d7425910d28
habeanf <habeanf@d4f3a8b0-3d31-816e-ec68-1c5367baaf3b>
2010-12-08 17:50:08 +0000

    First version. Contains reading loop and outline for addEdge and addPolygon.

---
+++  jhalfedge --username berger.itamar/src/parser/HalfEdgeReader.java
@@ -0,0 +1,140 @@
+package parser;
+
+import model.Face;
+import model.HalfEdge;
+import model.HalfEdgeDataStructure;
+import model.Vertex;
+
+import java.io.FileReader;
+import java.util.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: habeanf
+ * Date: Dec 8, 2010
+ * Time: 7:20:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HalfEdgeReader {
+    Map<Edge, HalfEdge> halfEdgeMap;
+    List<Face> faces;
+    List<int[]> faceIds;
+    Map<Integer, Vertex> vertexMap;
+
+    public Edge addEdge(int fromVertex, int toVertex) {
+        
+        return new Edge(fromVertex,toVertex);
+    }
+
+    public Face addFace(List<HalfEdge> halfEdgeLoop) {
+        return new Face(new HalfEdge(new Vertex(0,0,0,0)));
+    }
+
+    public HalfEdgeDataStructure get(String path, boolean normalizeVertices) {
+        // initialize structures for reading from file
+        vertexMap   = new HashMap<Integer,Vertex>();
+        halfEdgeMap = new HashMap<Edge,HalfEdge>();
+        faceIds     = new ArrayList<int[]>();
+
+        // used for normalization vertices
+        float   max = 0;
+
+        // get reader
+        MeshReader meshReader = getReader(path);
+        try {
+            FileReader      fileReader   = new FileReader(path);
+            OBJLineIterator lineIterator = new OBJLineIterator(fileReader);
+
+            meshReader.preProcess(lineIterator);
+
+            while (lineIterator.hasNext()) {
+                final String[] fields = lineIterator.nextLine().split("\\s+");
+                if (meshReader.isVertex(fields)) {
+                    // vertex constructor initializes vertex.half_ = null
+                    final Vertex vertex = meshReader.getVertex(fields);
+                    max = findmax(max,vertex);
+                    vertexMap.put(vertex.getId(), vertex);
+                } else if (meshReader.isFace(fields)) {
+                    faceIds.add(meshReader.getFaceIds(fields));
+                }
+            }
+            lineIterator.close();
+            fileReader.close();
+
+            // normalize vertices
+            if(normalizeVertices) {
+                for (Vertex vertex : vertexMap.values()) {
+                    vertex.normalize(max);
+                }
+            }
+
+            // create face array
+            faces = new ArrayList<Face>(faceIds.size());
+            
+            int[] curr_ids;
+            for (int i=0;i<faceIds.size();++i) {
+                curr_ids = faceIds.get(i);
+
+                /* create edges by iterating over vertex ids pair-wise */
+
+                // faceEdges will contain the halfe edges of the polygon created/retrieved by
+                // addEdge
+                List<HalfEdge> faceEdges = new ArrayList<HalfEdge>(curr_ids.length);
+                for (int j=0;j<curr_ids.length;++j) {
+                    int      vertexFrom  = curr_ids.get(j);
+                    int      vertexTo    = curr_ids.get((j+1)%curr_ids.length);
+
+                    Edge     edge        = addEdge(vertexFrom,vertexTo);
+                    HalfEdge half        = halfEdgeMap.get(edge);
+                    
+                    faceEdges.set(j,half);
+                }
+
+                // add face
+                addFace(faceEdges);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return new HalfEdgeDataStructure(halfEdgeMap.values(), faces, vertexMap);
+    }
+    
+    public class Edge {
+        int from;
+        int to;
+
+        public Edge(int from, int to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Edge edge = (Edge) o;
+
+            if (from != edge.from) return false;
+            if (to != edge.to) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = from;
+            result = 31 * result + to;
+            return result;
+        }
+    }
+
+    private MeshReader getReader(String path) {
+        if (path.endsWith("obj")) {
+            return new OBJReader();
+        }
+        return new OFFReader();
+    }
+}
